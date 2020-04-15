3f37ed14d04a35d4d8ce40030f36795badf72c59
Rick Fencl <richardfencl@msn.com>
2020-04-15 15:28:26 -0700

    Added wind dir and speed

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -12,6 +12,27 @@ const formatTime = (t) => {
   return `${h}:${m} ${ampm}`;
 };
 
+const getDirection = (d) => {
+  let directions = ['NE', 'E', 'SE', 'S', 'SW', 'W', 'NW', 'N'];
+  let i = 0;
+  if (d > 337 || d < 23) {
+    i = 7;
+  } else if (d > 293 && d <= 337) {
+    i = 6;
+  } else if (d > 247 && d <= 293) {
+    i = 5;
+  } else if (d > 202 && d <= 247) {
+    i = 4;
+  } else if (d > 157 && d <= 202) {
+    i = 3;
+  } else if (d > 112 && d <= 157) {
+    i = 2;
+  } else if (d > 67 && d <= 112) {
+    i = 1;
+  }
+
+  return directions[i];
+};
 
 const formatWeatherResponse = ({ code, error, currently, daily }) => {
   if (code) {
@@ -19,12 +40,13 @@ const formatWeatherResponse = ({ code, error, currently, daily }) => {
   }
   const currentWeatherData = currently;
 
-   const highTime = formatTime(
-     new Date(daily.data[0].temperatureHighTime * 1000)
-   );
-   const lowTime = formatTime(
-     new Date(daily.data[0].temperatureLowTime * 1000)
-   );
+  const highTime = formatTime(
+    new Date(daily.data[0].temperatureHighTime * 1000)
+  );
+  const lowTime = formatTime(new Date(daily.data[0].temperatureLowTime * 1000));
+
+  const bearing = getDirection(currently.windBearing);
+  const speed = currently.windSpeed;
 
   return `${daily.data[0].summary} It is currently ${
     currentWeatherData.temperature
@@ -34,9 +56,10 @@ const formatWeatherResponse = ({ code, error, currently, daily }) => {
     daily.data[0].temperatureLow
   )} degrees at ${lowTime} with a high of ${Math.round(
     daily.data[0].temperatureHigh
-  )} degrees at ${highTime}.`;
+  )} degrees at ${highTime}. The wind is from the ${bearing} blowing at ${
+    currently.windSpeed
+  } miles/hour.`;
 };
-
 
 // returns a promise
 const getWeatherInfo = ({ latitude, longitude }, callback) => {
